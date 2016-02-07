6a2890ab4189ecb124fd012dd83ef09b50f0585c
weizilim <weizilim@gmail.com>
2016-02-07 15:58:19 +0000

    plot1 code

---
+++ plot1.R
@@ -0,0 +1,19 @@
+library(data.table)
+library(dplyr)
+
+#read data
+data <- fread("household_power_consumption.txt", sep=";")
+
+#subset data
+data <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
+
+#convert to datetime
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data$datetime <- paste0(data$Date," ",data$Time)
+data$datetime <- as.POSIXct(strptime(data$datetime, "%Y-%m-%d %H:%M:%S"))
+
+data$Global_active_power <- as.numeric(data$Global_active_power)
+
+png("plot1.png")
+hist(data$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+dev.off()
